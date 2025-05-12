59e0cef6432bbca07d7e61a8810467001001a0f4
Daniel Perez <d_perez3@mail.fhsu.edu>
2025-05-12 10:56:23 -0500

    altered a route

--- routes/authorizeroutes.js
+++ routes/authorizeroutes.js
@@ -1,7 +1,7 @@
 //handles router logic for registering user
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser} = require('../controls/authorizecontroller');
+const {registerUser} = require('../controls/authorizecontroller');
 
 // Register route
 router.post('/register', registerUser);
